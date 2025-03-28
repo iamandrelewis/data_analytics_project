0170e501ada4d450271338bf15cf82c9ac4094ec
Andre Lewis <iamandrelewis@gmail.com>
2025-03-28 15:34:04 -0500

    bug fixes - update climb and fall functions in players class

--- main/game.py
+++ main/game.py
@@ -3,29 +3,43 @@ import os
 
 class player:
     """
-    mkf
+    
     """
     def __init__(self,name, score,position):
         self.name = name
         self.score = score
         self.position = position
 
-    def climb(*args,x:int) -> int:
+    def climb(self,x):
         """
         Nla
         """
-        print(args)
-        if(x == 3):
-            x = 24
-        elif (x == 14):
-            x = 42
-        elif (30):
-            x = 86
-        return x
-    def fall(*args,y:int) -> int:
-        print(args)
-        return y
-
+        if(x == 3 and self.position < 24):
+            self.position = 24
+        elif (x == 14 and self.position < 42):
+            self.position = 42
+        elif (x == 30 and self.position < 86):
+           self.position = 86
+        else:
+            self.position += x
+    def fall(self,y:int) -> int:
+        """
+        Nla
+        """
+        if(y == 27):
+            self.position = 7
+        elif (y == 20):
+            self.position = 1
+        elif (y == 89):
+           self.position = 5
+        elif (y == 57):
+           self.position = 29
+        elif (y == 76):
+           self.position = 44
+        elif (y == 97):
+           self.position = 2
+        else:
+            self.position += 0
 
 class game:
     l = []
@@ -34,7 +48,7 @@ class game:
     def __init__(self):
         pass
     def board(l:list):
-        print("SNAKES & LADDERS")
+        print("SNAKES & LADDERS\t\t\t\tCreated by Andre Lewis (2108069)\n\n")
         turn = 2    
         for i in range(0,100):
             l.insert(i,i+1)
@@ -48,56 +62,65 @@ class game:
                 print("-------------------------------------------------------------------------")
                 turn +=1
     def ladders():
-        print("Ladders\n 3-24 \n 14-42\n 30-86\n 37-57\n 50-96\n 66-74\n")
-    def snakes(): 
-        print("Snakes\n 3-24 \n 14-42\n 30-86\n 37-57\n 50-96\n 66-74\n")
+        """
+        """
+        print("Ladders\n 3 -> 24 \n 14 -> 42\n 30 -> 86\n 37 -> 57\n 50 -> 96\n 66 -> 74\n")
+    def snakes():
+        """
+        """
+        print("Snakes\n 27 -> 7 \n 20 -> 1\n 89 -> 5\n 57 -> 29\n 76 -> 44\n 97 -> 2\n")
     def dice_roll() -> int:
+        """
+        """
         import random
         dice =[1,2,3,4,5,6]
         return random.choice(dice)
     def start(self) -> int:
-        os.system('cls')
-        """        try:"""
-        playersList = {}
-        numPlayers = int(input("Enter the number of players: "))
-        if (numPlayers <= 4 and numPlayers > 1):
-            for x in range(numPlayers):
-                playerName = input(f'Enter the name of Player #{x+1}: ')
-                playersList.update({
-                    f'player{x}': player(playerName,0,0),
-                })
-            while(True):
-                for p in playersList:
-                    os.system('cls')
-                    self.board(self.l)
-                    try:
-                        choice = int(input(f'{playersList[p].name}: Enter "1" to roll the dice ->  '))
-                        if(choice == 1):
-                            dice = self.dice_roll()
-                            playersList[p].position += dice   
-                            print("Dice: ",dice, "Before climb")
-                            playersList[p].position = playersList[p].climb(x=dice)
-                            print(f'{playersList[p].name} is at position {playersList[p].position}')
-                            print(dice, "After fall")
-                            playersList[p].position = playersList[p].fall(y=dice)
-                            print(f'{playersList[p].name} is at position {playersList[p].position}')
-                            os.system('pause')
-                        else:
-                            print('Enter "1" to proceed!....')
-                            os.system('pause')
-                            continue
-                    except:
-                        reversed(p)
-                if(playersList[p].position >= 100):
-                    break              
-        else:
-            print('Enter a valid number!')
-            os.system('pause')
-            self.start(self=game)
-        """        except:
-                    os.system('cls')
-                    self.start(self=game)
         """
+        """
+        os.system('cls')
+        try:
+            playersList = {}
+            numPlayers = int(input("Enter the number of players: "))
+            if (numPlayers <= 4 and numPlayers > 1):
+                for x in range(numPlayers):
+                    playerName = input(f'Enter the name of Player #{x+1}: ')
+                    playersList.update({
+                        f'player{x}': player(playerName,0,0),
+                    })
+                while(True):
+                    for p in playersList:
+                        os.system('cls')
+                        self.board(self.l)
+                        self.snakes()
+                        self.ladders()
+                        try:
+                            choice = int(input(f'{playersList[p].name}: Enter "1" to roll the dice ->  '))
+                            if(choice == 1):
+                                dice = self.dice_roll()
+                                print("Dice: ",dice)
+                                playersList[p].climb(x=dice)
+                                playersList[p].fall(y=dice)
+                                print(f'{playersList[p].name} is at position {playersList[p].position}')
+                                os.system('pause')
+                            else:
+                                print('Enter "1" to proceed!....')
+                                os.system('pause')
+                                continue
+                        except:
+                            reversed(p)
+                    if(playersList[p].position >= 100):
+                        print("Player ",playersList[p].name, " wins!")
+                        break              
+            else:
+                print('Enter a valid number!')
+                os.system('pause')
+                self.start(self=game)
+        except Exception as e:
+                    print(e)
+                    os.system('pause')
+                    self.start(self=game)
+        
 
 
 if __name__ == "__main__":
